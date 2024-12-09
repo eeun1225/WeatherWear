c9df5bea799c8ace98d9f169e655d39a407c59b1
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:43:03 +0900

    기능: 게시글 생성 요청 DTO 추가

--- src/main/java/com/spring/WeatherWear/board/domain/dto/BoardCreateRequest.java
+++ src/main/java/com/spring/WeatherWear/board/domain/dto/BoardCreateRequest.java
@@ -1,4 +1,24 @@
 package com.spring.WeatherWear.board.domain.dto;
 
+import com.spring.WeatherWear.board.domain.entity.Board;
+import com.spring.WeatherWear.User.member.entity.Member;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
 public class BoardCreateRequest {
+
+    private String title;
+    private String body;
+    private MultipartFile uploadImage;
+
+    public Board toEntity(Member member) {
+        return Board.builder()
+                .member(member)
+                .title(title)
+                .body(body)
+                .likeCnt(0)
+                .commentCnt(0)
+                .build();
+    }
 }
