2b3e246e6857f571ba3a368b027c64b8fee681d8
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:44:35 +0900

    기능: 좋아요 엔티티 구현

--- src/main/java/com/spring/WeatherWear/board/domain/entity/Like.java
+++ src/main/java/com/spring/WeatherWear/board/domain/entity/Like.java
@@ -1,4 +1,28 @@
 package com.spring.WeatherWear.board.domain.entity;
 
+import com.spring.WeatherWear.User.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Table(name = "\"like\"")
 public class Like {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;      // 좋아요를 누른 유저
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board board;    // 좋아요가 추가된 게시글
+
 }
