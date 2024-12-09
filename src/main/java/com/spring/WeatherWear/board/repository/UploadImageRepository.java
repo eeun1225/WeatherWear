bbf31f3931575eccde9651ef7d08d5467911131d
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:51:25 +0900

    feat: 업로드 이미지 레포지토리 인터페이스 구현

--- src/main/java/com/spring/WeatherWear/board/repository/UploadImageRepository.java
+++ src/main/java/com/spring/WeatherWear/board/repository/UploadImageRepository.java
@@ -1,4 +1,9 @@
 package com.spring.WeatherWear.board.repository;
 
-public class UploadImageRepository {
+import com.spring.WeatherWear.board.domain.entity.UploadImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UploadImageRepository extends JpaRepository<UploadImage, Long> {
 }
