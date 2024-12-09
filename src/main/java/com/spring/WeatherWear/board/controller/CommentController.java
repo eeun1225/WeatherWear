a989f7d799b4e520f29f550942cfbe21358deca1
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:41:50 +0900

    기능: 댓글 컨트롤러 구현

--- src/main/java/com/spring/WeatherWear/board/controller/CommentController.java
+++ src/main/java/com/spring/WeatherWear/board/controller/CommentController.java
@@ -1,4 +1,44 @@
 package com.spring.WeatherWear.board.controller;
 
+import com.spring.WeatherWear.board.domain.dto.CommentCreateRequest;
+import com.spring.WeatherWear.board.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/comments")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{boardId}")
+    public String addComments(@PathVariable Long boardId, @ModelAttribute CommentCreateRequest req,
+                              Authentication auth, Model model) {
+        commentService.writeComment(boardId, req, auth.getName());
+
+        model.addAttribute("message", "댓글이 추가되었습니다.");
+        model.addAttribute("nextUrl", "/boards/" + boardId);
+        return "printMessage";
+    }
+
+    @PostMapping("/{commentId}/edit")
+    public String editComment(@PathVariable Long commentId, @ModelAttribute CommentCreateRequest req,
+                              Authentication auth, Model model) {
+        Long boardId = commentService.editComment(commentId, req.getBody(), auth.getName());
+        model.addAttribute("message", boardId == null ? "잘못된 요청입니다." : "댓글이 수정 되었습니다.");
+        model.addAttribute("nextUrl", "/boards/" + boardId);
+        return "printMessage";
+    }
+
+    @GetMapping("/{commentId}/delete")
+    public String deleteComment(@PathVariable Long commentId, Authentication auth, Model model) {
+        Long boardId = commentService.deleteComment(commentId, auth.getName());
+        model.addAttribute("message", boardId == null ? "작성자만 삭제 가능합니다." : "댓글이 삭제 되었습니다.");
+        model.addAttribute("nextUrl", "/boards/" + boardId);
+        return "printMessage";
+    }
 }
