c0aaeb724ef52791186d9e9a34b16c80a6f0b0eb
eeun1225 <eeun9108@gmail.com>
2024-12-09 23:54:42 +0900

    fix: 로컬 회원가입, 로그인 DB 연결 오류 해결

--- src/main/java/com/spring/WeatherWear/member/dto/SignUpRequest.java
+++ src/main/java/com/spring/WeatherWear/member/dto/SignUpRequest.java
@@ -1,13 +1,28 @@
 package com.spring.WeatherWear.member.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.spring.WeatherWear.member.entity.AuthType;
+import com.spring.WeatherWear.member.entity.Member;
+import com.spring.WeatherWear.member.entity.Role;
+import lombok.*;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class SignUpRequest {
     private String email;
     private String password;
     private String name;
-    private String nickName;
+    private String nick_name;
+
+    public Member toEntity() {
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .nickName(nick_name)
+                .role(Role.USER)
+                .authType(AuthType.LOCAL)
+                .socialType(null)
+                .social_id(null)
+                .build();
+    }
 }
