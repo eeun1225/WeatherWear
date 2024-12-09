ce42e50e79c963bfd04e4c5b4b42475e1505f8cc
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:43:49 +0900

    기능: 댓글 생성 요청 DTO 추가

--- src/main/java/com/spring/WeatherWear/board/domain/dto/CommentCreateRequest.java
+++ src/main/java/com/spring/WeatherWear/board/domain/dto/CommentCreateRequest.java
@@ -1,4 +1,21 @@
 package com.spring.WeatherWear.board.domain.dto;
 
+import com.spring.WeatherWear.board.domain.entity.Board;
+import com.spring.WeatherWear.board.domain.entity.Comment;
+import com.spring.WeatherWear.User.member.entity.Member;
+import lombok.Data;
+
+@Data
 public class CommentCreateRequest {
+
+    private String body;
+
+    //, User user
+    public Comment toEntity(Board board, Member member) {
+        return Comment.builder()
+                .member(member)
+                .board(board)
+                .body(body)
+                .build();
+    }
 }
