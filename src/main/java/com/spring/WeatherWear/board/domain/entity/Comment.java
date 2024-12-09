01b6d19632023e39a4a6dd665861ad9dd40d9468
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:44:20 +0900

    기능: 댓글 엔티티 구현

--- src/main/java/com/spring/WeatherWear/board/domain/entity/Comment.java
+++ src/main/java/com/spring/WeatherWear/board/domain/entity/Comment.java
@@ -1,4 +1,33 @@
 package com.spring.WeatherWear.board.domain.entity;
 
-public class Comment {
+import com.spring.WeatherWear.board.domain.BaseEntity;
+import com.spring.WeatherWear.User.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Comment extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;      // 작성자
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board board;    // 댓글이 달린 게시판
+
+    public void update(String newBody) {
+        this.body = newBody;
+    }
 }
