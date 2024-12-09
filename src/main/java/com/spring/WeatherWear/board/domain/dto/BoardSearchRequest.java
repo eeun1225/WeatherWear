758de6d00aecc27179a475e3c966a20f395edebb
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:43:26 +0900

    기능: 게시글 검색 요청 DTO 추가

--- src/main/java/com/spring/WeatherWear/board/domain/dto/BoardSearchRequest.java
+++ src/main/java/com/spring/WeatherWear/board/domain/dto/BoardSearchRequest.java
@@ -1,4 +1,13 @@
 package com.spring.WeatherWear.board.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class BoardSearchRequest {
+
+    private String sortType;
+    private String searchType;
+    private String keyword;
 }
