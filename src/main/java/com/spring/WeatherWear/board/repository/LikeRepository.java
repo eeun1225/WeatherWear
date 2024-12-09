6e03cb6bf9b1e5226498e403ca5f26686ee96ff3
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:51:11 +0900

    feat: 좋아요 레포지토리 인터페이스 구현

--- src/main/java/com/spring/WeatherWear/board/repository/LikeRepository.java
+++ src/main/java/com/spring/WeatherWear/board/repository/LikeRepository.java
@@ -1,4 +1,14 @@
 package com.spring.WeatherWear.board.repository;
 
-public class LikeRepository {
+import com.spring.WeatherWear.board.domain.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    void deleteByMemberEmailAndBoardId(String loginId, Long boardId);
+    Boolean existsByMemberEmailAndBoardId(String loginId, Long boardId);
+    List<Like> findAllByMember_Email(String loginId);
 }
