b26c206c2d82e9b94dd90de0455a462260c46674
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 05:04:49 +0900

    feat: Content 도메인 클래스 추가

--- src/main/java/com/spring/WeatherWear/domain/Content.java
+++ src/main/java/com/spring/WeatherWear/domain/Content.java
@@ -1,4 +1,16 @@
 package com.spring.WeatherWear.domain;
 
+import lombok.Data;
+
+@Data
 public class Content {
+
+    private int id;
+    private String title;
+    private String texts;
+
+    private String writer;
+    private String password;
+
+    private String updateDate;
 }
