da656fd56265a28279686bb9b29a883f88b44a13
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:50:42 +0900

    feat: 게시글 레포지토리 인터페이스 구현

--- src/main/java/com/spring/WeatherWear/board/repository/BoardRepository.java
+++ src/main/java/com/spring/WeatherWear/board/repository/BoardRepository.java
@@ -1,4 +1,25 @@
 package com.spring.WeatherWear.board.repository;
 
-public class BoardRepository {
+import com.spring.WeatherWear.board.domain.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    // 제목으로 검색하며 페이징 처리
+    Page<Board> findAllByTitleContains(String title, Pageable pageable);
+
+    // 작성자 닉네임으로 검색하며 페이징 처리
+    Page<Board> findAllByMember_NickNameContains(String nickname, Pageable pageable);
+
+    // 특정 사용자가 작성한 모든 게시글 조회
+    List<Board> findAllByMember_Email(String loginId);
 }
+
+
