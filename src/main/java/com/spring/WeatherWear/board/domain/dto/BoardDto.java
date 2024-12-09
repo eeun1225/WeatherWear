8af80fa964ec3ea165e3315175842d535a8f25c5
ooheunsu <126392227+ooheunsu@users.noreply.github.com>
2024-12-10 04:43:15 +0900

    기능: 게시글 DTO 추가

--- src/main/java/com/spring/WeatherWear/board/domain/dto/BoardDto.java
+++ src/main/java/com/spring/WeatherWear/board/domain/dto/BoardDto.java
@@ -1,4 +1,39 @@
 package com.spring.WeatherWear.board.domain.dto;
 
+import com.spring.WeatherWear.board.domain.entity.Board;
+import com.spring.WeatherWear.board.domain.entity.UploadImage;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class BoardDto {
+
+    private Long id;
+    private String userLoginId;
+    private String userNickname;
+    private String title;
+    private String body;
+    private Integer likeCnt;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastModifiedAt;
+    private MultipartFile newImage;
+    private UploadImage uploadImage;
+
+    public static BoardDto of(Board board) {
+        return BoardDto.builder()
+                .id(board.getId())
+                .userLoginId(board.getMember().getEmail())
+                .userNickname(board.getMember().getNickName())
+                .title(board.getTitle())
+                .body(board.getBody())
+                .createdAt(board.getCreatedAt())
+                .lastModifiedAt(board.getLastModifiedAt())
+                .likeCnt(board.getLikes().size())
+                .uploadImage(board.getUploadImage())
+                .build();
+    }
 }
