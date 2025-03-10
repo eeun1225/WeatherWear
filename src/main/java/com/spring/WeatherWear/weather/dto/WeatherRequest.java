b712a84d2cc0ef496c0e4651bcdb8723427b2bb1
eeun1225 <eeun9108@gmail.com>
2025-03-10 16:08:15 +0900

    refactor: WeatherRequest 생성자 수정

--- src/main/java/com/spring/WeatherWear/weather/dto/WeatherRequest.java
+++ src/main/java/com/spring/WeatherWear/weather/dto/WeatherRequest.java
@@ -4,6 +4,10 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 @Data
 @Getter
 @Setter
@@ -15,4 +19,23 @@ public class WeatherRequest {
     private String base_date;
     private String base_time;
     private AreaRequest areaRequest;
+
+    public WeatherRequest(String serviceKey, AreaRequest areaRequest) {
+        this.serviceKey = serviceKey;
+        this.pageNo = "1";
+        this.numOfRows = "1000";
+        this.dataType = "JSON";
+        this.base_date = currentDate();
+        this.base_time = currentTime();
+        this.areaRequest = areaRequest;
+    }
+
+    private String currentDate(){
+        return LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
+    }
+
+    private String currentTime(){
+        return LocalTime.now().format(DateTimeFormatter.ofPattern("HH00"));
+    }
+
 }
--- src/main/java/com/spring/WeatherWear/weather/service/WeatherService.java
+++ src/main/java/com/spring/WeatherWear/weather/service/WeatherService.java
@@ -5,7 +5,9 @@ import com.spring.WeatherWear.weather.dto.*;
 import com.spring.WeatherWear.weather.entity.WeatherArea;
 import com.spring.WeatherWear.weather.repository.WeatherAreaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDate;
@@ -19,23 +21,20 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class WeatherService {
-    private final String serviceKey = "";
     private final WeatherAreaRepository weatherAreaRepository;
 
+    @Value("${service.api.key}")
+    private String serviceKey;
+
+    @Value("${base.api.url}")
+    private String baseUrl;
+
+    @Value("${api.url}")
+    private String apiUrl;
+
     public WeatherApiResponse getWeather(AreaRequest areaRequest){
-        WebClient webClient = WebClient.builder()
-                .baseUrl("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0").build();
-
-        WeatherRequest weatherRequest = new WeatherRequest();
-        weatherRequest.setServiceKey(serviceKey);
-        weatherRequest.setPageNo("1");
-        weatherRequest.setNumOfRows("1000");
-        weatherRequest.setDataType("JSON");
-        weatherRequest.setBase_date(currentDate());
-        weatherRequest.setBase_time(currentTime());
-        weatherRequest.setAreaRequest(areaRequest);
-
-        String apiUrl = "";
+        WebClient webClient = WebClient.builder().baseUrl(baseUrl).build();
+        WeatherRequest weatherRequest = new WeatherRequest(serviceKey, areaRequest);
 
         WeatherApiResponse weatherApiResponse = webClient.get()
                 .uri(apiUrl, weatherRequest.getServiceKey(),
@@ -53,32 +52,22 @@ public class WeatherService {
         return weatherApiResponse;
     }
 
-    public String currentDate(){
-        return LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE);
-    }
-
-    public String currentTime(){
-        return LocalTime.now().format(DateTimeFormatter.ofPattern("HH00"));
-    }
-
-
     public AreaRequest getLocation(AreaRequest areaRequest) {
         Optional<WeatherArea> nearestArea = weatherAreaRepository.findNearestArea(areaRequest.getLongitude(), areaRequest.getLatitude());
 
-        if (nearestArea.isPresent()) {
-            WeatherArea area = nearestArea.get();
-
-            areaRequest.setAreacode(area.getAreaCode());
-            areaRequest.setStep1(area.getStep1());
-            areaRequest.setStep2(area.getStep2());
-            areaRequest.setStep3(area.getStep3());
-            areaRequest.setNx(area.getGridX());
-            areaRequest.setNy(area.getGridY());
-
-            return areaRequest;
-        } else {
+        if (nearestArea.isEmpty()) {
             throw new RuntimeException("가까운 지역을 찾을 수 없습니다.");
         }
+
+        WeatherArea area = nearestArea.get();
+        areaRequest.setAreacode(area.getAreaCode());
+        areaRequest.setStep1(area.getStep1());
+        areaRequest.setStep2(area.getStep2());
+        areaRequest.setStep3(area.getStep3());
+        areaRequest.setNx(area.getGridX());
+        areaRequest.setNy(area.getGridY());
+
+        return areaRequest;
     }
 
     public WeatherData extractWeatherData(WeatherApiResponse response, AreaRequest areaRequest) {
@@ -87,8 +76,8 @@ public class WeatherService {
         }
 
         List<WeatherItemDTO> items = response.getResponse().getBody().getItems().getItem();
-
         Map<String, String> weatherData = new HashMap<>();
+
         for (WeatherItemDTO item : items) {
             weatherData.put(item.getCategory(), String.valueOf(item.getObsrValue()));
         }
@@ -97,7 +86,6 @@ public class WeatherService {
         double windSpeed = Double.parseDouble(weatherData.getOrDefault("WSD", "0"));
         double precipitation = Double.parseDouble(weatherData.getOrDefault("RN1", "0"));
         double humidityValue = Double.parseDouble(weatherData.getOrDefault("REH", "0"));
-
         int humidity = (int) Math.round(humidityValue);
 
         return new WeatherData(temperature, windSpeed, precipitation, humidity, areaRequest);
